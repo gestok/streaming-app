17ae8dcc0788022ed9406948fb67e54ca92fc6fc
gestok <giorgos.cho@hotmail.com>
2022-11-09 10:16:58 +0200

    1 file modified

--- src/data.js
+++ src/data.js
@@ -76,42 +76,56 @@ export const lives = [
     id: 'ESL_SC2',
     logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/ESL_SC2.png',
     thumb:
-      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/banners/esl_sc2.jpg',
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live1.jpg',
     viewers: Math.floor(Math.random() * 500) + 10,
   },
   {
     id: 'OgamingSC2',
     logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/OG_SC2.png',
     thumb:
-      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/banners/og-sc2.jpg',
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live2.jpg',
     viewers: Math.floor(Math.random() * 500) + 10,
   },
   {
     id: 'cretetion',
     logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/cretetion.png',
     thumb:
-      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/banners/cretetion.jpg',
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live3.jpg',
     viewers: Math.floor(Math.random() * 500) + 10,
   },
   {
     id: 'freecodecamp',
     logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/freecodecamp.png',
     thumb:
-      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/banners/freecodecamp.jpg',
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live4.jpg',
+    viewers: Math.floor(Math.random() * 500) + 10,
+  },
+  {
+    id: 'storbeck',
+    logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/offline.jpg',
+    thumb:
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live5.jpg',
+    viewers: Math.floor(Math.random() * 500) + 10,
+  },
+  {
+    id: 'habathcx',
+    logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/offline.jpg',
+    thumb:
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live6.jpg',
     viewers: Math.floor(Math.random() * 500) + 10,
   },
   {
     id: 'RobotCaleb',
     logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/RobotCaleb.png',
     thumb:
-      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/banners/code.jpg',
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live7.jpg',
     viewers: Math.floor(Math.random() * 500) + 10,
   },
   {
     id: 'noobs2ninjas',
     logo: 'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/streamers/noobs2ninjas.png',
     thumb:
-      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/banners/codespiral.jpg',
+      'https://raw.githubusercontent.com/gestok/streaming-app/main/src/assets/thumbs/lives/live8.jpg',
     viewers: Math.floor(Math.random() * 500) + 10,
   },
 ];
